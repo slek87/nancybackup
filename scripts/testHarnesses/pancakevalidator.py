e2a88a1ad48253a538fa72da331fe1dc9a31c2d8
sailekyang <sailekyang@gmail.com>
2019-03-17 16:37:04 -0400

    clean up

--- scripts/testHarnesses/pancakevalidator.py
+++ scripts/testHarnesses/pancakevalidator.py
@@ -4,26 +4,43 @@ import copy
 
 start = []
 end = []
-i = 0
+read = True
 algo = 0
 algoname = ['astar', 'fhat', 'risk', 'lsslrta']
 
 for line in fileinput.input():
-    if i == 0:
+    if line[0] == '{':
+        # A
+        if (start != end):
+            print(algoname[algo], 'wrong!')
+        algo += 1
+        read = True
+        break
+    elif read:
+        # B
         s = re.split(r'\s+',line)
         for num in s:
             if len(num) > 0:
                 start.append(int(num))
-        i += 1
         end = copy.deepcopy(start)
         end.sort()
+        read = False
     elif len(line) < 6:
         indx = int(line)
         start[0:indx+1] = start[0:indx+1][::-1]
     else:
+        # A
         if (start != end):
             print(algoname[algo], 'wrong!')
         algo += 1
-        i = 0
         start.clear()
         end.clear()
+
+        # B
+        s = re.split(r'\s+',line)
+        for num in s:
+            if len(num) > 0:
+                start.append(int(num))
+        end = copy.deepcopy(start)
+        end.sort()
+        
