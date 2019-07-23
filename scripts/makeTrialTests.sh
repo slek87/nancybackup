80c0e28afcd9185c64a9149b82536719fefdd09c
sailekyang <sailekyang@gmail.com>
2019-07-23 12:52:58 -0400

    static

--- scripts/makeTrialTests.sh
+++ scripts/makeTrialTests.sh
@@ -2,9 +2,15 @@
 
 if (($# == 1))
 then
-    outFile=$1
-    g++ -std=c++11 ../cpp/trialBased/trialbasedTestRT.cpp -o ../../build_release/${outFile}
-    chmod a+x ../../build_release/${outFile}
+    arg=$1
+    if [ "$arg" = "static" ] 
+        g++ -std=c++11 -static ../cpp/trialBased/trialbasedTestRT.cpp -o ../../build_release/trialBasedTest
+        chmod a+x ../../build_release/trialBasedTest
+    else
+        outFile=$1
+        g++ -std=c++11 ../cpp/trialBased/trialbasedTestRT.cpp -o ../../build_release/${outFile}
+        chmod a+x ../../build_release/${outFile}
+    fi
 else
     g++ -std=c++11 ../cpp/trialBased/trialbasedTestRT.cpp -o ../../build_release/trialBasedTest
     chmod a+x ../../build_release/trialBasedTest
