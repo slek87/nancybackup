065e8e541c31cf60b7dad4e376549321c34a7bd5
sailekyang <sailekyang@gmail.com>
2019-08-02 20:55:47 -0400

    optimized flag

--- scripts/makeTrialTests.sh
+++ scripts/makeTrialTests.sh
@@ -5,14 +5,14 @@ then
     arg=$1
     if [ "$arg" = "static" ] 
     then
-        g++ -std=c++11 -static ../cpp/trialBased/trialbasedTestRT.cpp -o ../../build_release/trialBasedTest
+        g++ -std=c++11 -O3 -static ../cpp/trialBased/trialbasedTestRT.cpp -o ../../build_release/trialBasedTest
         chmod a+x ../../build_release/trialBasedTest
     else
         outFile=$1
-        g++ -std=c++11 ../cpp/trialBased/trialbasedTestRT.cpp -o ../../build_release/${outFile}
+        g++ -std=c++11 -O3 ../cpp/trialBased/trialbasedTestRT.cpp -o ../../build_release/${outFile}
         chmod a+x ../../build_release/${outFile}
     fi
 else
-    g++ -std=c++11 ../cpp/trialBased/trialbasedTestRT.cpp -o ../../build_release/trialBasedTest
+    g++ -std=c++11 -O3 ../cpp/trialBased/trialbasedTestRT.cpp -o ../../build_release/trialBasedTest
     chmod a+x ../../build_release/trialBasedTest
 fi
