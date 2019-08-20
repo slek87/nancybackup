131581862dbb58f518c1ebc804d641135cfe1ec2
sailekyang <sailekyang@gmail.com>
2019-08-20 16:59:58 -0400

    IEPP

--- scripts/makeTests.sh
+++ scripts/makeTests.sh
@@ -11,12 +11,15 @@ then
         g++ -std=c++11 -O3 -static ../cpp/expansionTestIEP.cpp -o ../../build_release/IEPexpansionTest
         g++ -std=c++11 -O3 -static ../cpp/expansionTestLSSLRTA.cpp -o ../../build_release/LSSLRTAexpansionTest
         g++ -std=c++11 -O3 -static ../cpp/expansionTestRisk.cpp -o ../../build_release/RISKexpansionTest
+        g++ -std=c++11 -O3 -static ../cpp/expansionTestIEPP.cpp -o ../../build_release/IEPPexpansionTest
+
     fi
 else
     g++ -std=c++11 -O3  ../cpp/trialBased/trialbasedTestRT.cpp -o ../../build_release/trialBasedTest
     g++ -std=c++11 -O3  ../cpp/expansionTestFHat.cpp -o ../../build_release/FHATexpansionTest
     g++ -std=c++11 -O3  ../cpp/expansionTestIE.cpp -o ../../build_release/IEexpansionTest
     g++ -std=c++11 -O3  ../cpp/expansionTestIEP.cpp -o ../../build_release/IEPexpansionTest
+    g++ -std=c++11 -O3  ../cpp/expansionTestIEPP.cpp -o ../../build_release/IEPPexpansionTest
     g++ -std=c++11 -O3  ../cpp/expansionTestLSSLRTA.cpp -o ../../build_release/LSSLRTAexpansionTest
     g++ -std=c++11 -O3  ../cpp/expansionTestRisk.cpp -o ../../build_release/RISKexpansionTest
 
